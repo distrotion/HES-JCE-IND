88dc83488aa969c8837ab1658b4e238bb796dd89
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2022-08-30 01:05:55 +0700

    .

--- flow/002/INDUCTION.js
+++ flow/002/INDUCTION.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 let mongodb = require('../../function/mongodb');
-let mssql = require('../../function/mssql');
+let mssql = require('./../../function/mssql');
 
 const d = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });;
 let day = d;
@@ -23,7 +23,7 @@ router.post('/getINDCOIL', async (req, res) => {
     output = await mongodb.find(INDUCTION, COIL_EX, {});
   }
   catch (err) {
-      output = [];
+    output = [];
   }
 
   res.json(output);
@@ -44,42 +44,42 @@ router.post('/INDcoilUPDATE', async (req, res) => {
     let check = await mongodb.find(INDUCTION, COIL_EX, { "WID": input['WID'] });
 
     if (check.length === 0) {
-        let check2 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "COIL_NAME": input['COIL_NAME']}] });
+      let check2 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "COIL_NAME": input['COIL_NAME'] }] });
 
-        if (check2.length > 0) {
+      if (check2.length > 0) {
 
-        } else {
-            input['WID'] = `WIDA-${Date.now()}`;
-            input['COUNTER'] = '0';
-            input['STATUS'] = '';
-            var ins = await mongodb.insertMany(INDUCTION, COIL_EX, [input]);
-        }
+      } else {
+        input['WID'] = `WIDA-${Date.now()}`;
+        input['COUNTER'] = '0';
+        input['STATUS'] = '';
+        var ins = await mongodb.insertMany(INDUCTION, COIL_EX, [input]);
+      }
 
     } else {
-        //
-        let check2 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "COIL_NAME": input['COIL_NAME']}] });
+      //
+      let check2 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "COIL_NAME": input['COIL_NAME'] }] });
 
-        if(check2.length > 0){
-          
-        }else{
-          let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "COIL_NAME": input['COIL_NAME'], "PATTERN": input['PATTERN'] ,"LIMIT":  input['LIMIT'],"COIL_NO":  input['COIL_NO']} });
-        }
+      if (check2.length > 0) {
 
-        if(input['STATUS'] === 'ACTIVE'){
-          let check3 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "STATUS": 'ACTIVE',"COIL_NO": input['COIL_NO'],"PATTERN": input['PATTERN']}] });
-          if(check3.length === 0){
-            let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "STATUS": input['STATUS']} });
-          }
-        }else{
-          let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "STATUS": input['STATUS']} });
+      } else {
+        let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "COIL_NAME": input['COIL_NAME'], "PATTERN": input['PATTERN'], "LIMIT": input['LIMIT'], "COIL_NO": input['COIL_NO'] } });
+      }
+
+      if (input['STATUS'] === 'ACTIVE') {
+        let check3 = await mongodb.find(INDUCTION, COIL_EX, { $and: [{ "STATUS": 'ACTIVE', "COIL_NO": input['COIL_NO'], "PATTERN": input['PATTERN'] }] });
+        if (check3.length === 0) {
+          let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "STATUS": input['STATUS'] } });
         }
-        
+      } else {
+        let upd = await mongodb.update(INDUCTION, COIL_EX, { "WID": input['WID'] }, { $set: { "STATUS": input['STATUS'] } });
+      }
+
     }
 
     output = await mongodb.find(INDUCTION, COIL_EX, {});
   }
   catch (err) {
-      output = [];
+    output = [];
   }
   res.json(output);
 });
@@ -91,7 +91,7 @@ router.post('/getINDpattern', async (req, res) => {
     output = await mongodb.find(INDUCTION, COIL_MASTER, {});
   }
   catch (err) {
-      output = [];
+    output = [];
   }
 
   res.json(output);
@@ -110,27 +110,70 @@ router.post('/INDpatternUPDATE', async (req, res) => {
     let check = await mongodb.find(INDUCTION, COIL_MASTER, { "PID": input['PID'] });
 
     if (check.length === 0) {
-        let check2 = await mongodb.find(INDUCTION, COIL_MASTER, { $and: [{ "PATTERN": input['PATTERN']}] });
+      let check2 = await mongodb.find(INDUCTION, COIL_MASTER, { $and: [{ "PATTERN": input['PATTERN'] }] });
 
-        if (check2.length > 0) {
+      if (check2.length > 0) {
 
-        } else {
-            input['PID'] = `PIDA-${Date.now()}`;
-            var ins = await mongodb.insertMany(INDUCTION, COIL_MASTER, [input]);
-        }
+      } else {
+        input['PID'] = `PIDA-${Date.now()}`;
+        var ins = await mongodb.insertMany(INDUCTION, COIL_MASTER, [input]);
+      }
 
     } else {
-        //
-        let upd = await mongodb.update(INDUCTION, COIL_MASTER, { "PID": input['PID'] }, { $set: { "VOLT": input['VOLT'], "POWER": input['POWER'] , "TIME": input['TIME'] } });
+      //
+      let upd = await mongodb.update(INDUCTION, COIL_MASTER, { "PID": input['PID'] }, { $set: { "VOLT": input['VOLT'], "POWER": input['POWER'], "TIME": input['TIME'] } });
     }
 
     output = await mongodb.find(INDUCTION, COIL_MASTER, {});
   }
   catch (err) {
-      output = [];
+    output = [];
   }
   res.json(output);
 });
+
+
+router.post('/COILcheckdate', async (req, res) => {
+  //-------------------------------------
+  console.log(req.body);
+  let input = req.body;
+
+  //-------------------------------------
+  let output = [];
+  try {
+
+    query = `SELECT  *  FROM [PLANT_SUPPORT].[dbo].[HES_INDUCTION] where [date] between '${input['START']} 0:00:00' and '${input['END']} 23:59:59' order by [date] desc`
+
+    var db = await mssql.qurey(query);
+    if (db === `er`) {
+      return res.json({ "status": "nok", "note": "database error" });
+    }
+
+    let dataDB = db[`recordsets`][0]
+    resultDATA = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for (i = 0; i < dataDB.length; i++) {
+
+      for (j = 0; j < 20; j++) {
+        if (parseInt(dataDB[i][`F01`].toString()) - 1 == j) {
+          resultDATA[j]++;
+        }
+      }
+    }
+
+    output = resultDATA;
+
+  }
+  catch (err) {
+    output = [];
+  }
+
+  res.json(output);
+});
+
+
+
+
 
 
 module.exports = router;
